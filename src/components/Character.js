e37f99c9f37b0ad85f07dab8635e9b7a0e046d6c
Dominick Groven <36062458+DominickGroven@users.noreply.github.com>
2020-11-22 16:50:50 -0800

    Finished project

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div
+`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const StyledH2 = styled.h2`
+    border: solid black 3px;
+    padding: 10px 5px;
+    text-align: center;
+    color: black;
+    background-color: #EEF3BB;
+    
+`;
+function Character(props) {
+    return (
+        <StyledDiv>
+            <div>{props.characters.map(char => {
+                return <StyledH2 key={char.name}>{char.name} : {char.gender} : {char.status}</StyledH2>
+            })}
+            </div>
+        </StyledDiv>
+    )
+}
+
+export default Character; 
